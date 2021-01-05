a8305bf9fed5b1285275486f882a0383cfcda02e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-05 16:16:39 +0530

    XAMARIN-34581 - Sample and screenshot added.

---
+++ TreeViewXamarin/TreeViewXamarin/TreeViewXamarin/Helper/TextColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TreeViewXamarin
+{
+    #region TextColorConverter
+    public class TextColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = (int)value;
+            if (level == 0) return Color.Red;
+            else if (level == 1) return Color.Blue;
+            else return Color.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
